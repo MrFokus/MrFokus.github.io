90a6a300fd8a761466fdba774865834fcbed3df3
Глубоков Герман <glubokovgerman@MacBook-Pro.local>
2024-08-27 23:29:30 +0300

    feat: add mobile welcome section

--- components/features/SliderGallerySkills.vue
+++ components/features/SliderGallerySkills.vue
@@ -9,11 +9,12 @@ function getHeight(currentIndex: number,length:number, lowerBound?:number) {
     const height = (-Math.abs(currentIndex - Math.floor(length/2)) + length)*100/length
     return lowerBound && height <= lowerBound? lowerBound: height
 }
+
 </script>
 
 <template>
-    <ul class="flex slider-gallery-skills gap-8 items-end justify-center">
-        <li v-for="(skill,index) in skillsList" :style="{height: getHeight(index, skillsList.length)+'%'}" :key="index" class="card-skill">
+    <ul class="flex slider-gallery-skills gap-8 items-end justify-center white-theme overflow-hidden max-w-[100vw] relative z-30 mx-[-16px]">
+        <li v-for="(skill,index) in skillsList.slice(0, view)" :style="{height: getHeight(index, view)+'%'}" :key="index" class="card-skill">
             <img :src="skill" alt="">
         </li>
     </ul>
--- components/widgets/Welcome.vue
+++ components/widgets/Welcome.vue
@@ -2,20 +2,30 @@
 import FillingText from '../features/FillingText.vue';
 import SliderGallerySkills from '../features/SliderGallerySkills.vue';
 import Thinker from '@/assets/img/thinker.png'
+import Nuxt from '@/assets/img/nuxt.svg'
+import Ts from '@/assets/img/ts.svg'
+import Saas from '@/assets/img/saas.png'
 </script>
 
 <template>
-    <section class="welcome min-w-[100dvw] gap-16 flex-col">
-        <div class="name items-center gap-3 py-1">
-            <span class="point"/>
+    <section class="welcome min-w-[100dvw] gap-16 flex-col py-4">
+        <div class="name items-center gap-3 py-1 white-theme">
+            <span class="point" />
             <p class=" text-t-m">Герман Глубоков</p>
         </div>
-        <SliderGallerySkills :view="5" :skills-list="[Thinker,Thinker,Thinker,Thinker,Thinker]"/>
-        <div class="title">
-            <FillingText>
-                Я — Герман, и я программист! 
-            </FillingText>
+        <SliderGallerySkills class="h-[160px]" :view="3" :skills-list="[Ts, Nuxt, Saas]" />
+        <div class="gap-6 flex-col items-center py-8">
+            <div class="title flex-col gap-4">
+                <FillingText class="white-theme text-h1-m">
+                    Я — Герман, и я программист!
+                </FillingText>
+                <p class="white-theme text-h1-m">Программист с двухлетним стажем, создаю эффективные решения
+                    и качественные приложения. 
+                </p>
+            </div>
+            <img class="w-[194px] h-fit aspect-square" :src="Thinker" alt="">
         </div>
+
     </section>
 </template>
 
@@ -23,5 +33,4 @@ import Thinker from '@/assets/img/thinker.png'
 .welcome {
     background: linear-gradient(0deg, #5301DE 0%, #41158C 100%);
 }
-
 </style>
