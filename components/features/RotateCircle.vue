cef10088b002d7c13245524ca9661f3ef86ec398
Глубоков Герман <glubokovgerman@MacBook-Pro.local>
2024-08-30 00:53:40 +0300

    feat: add circle skills

---
+++ components/features/RotateCircle.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const itemsRef: Ref<HTMLElement | undefined> = ref()
+const containerRef: Ref<HTMLElement | undefined> = ref()
+const items: Ref<HTMLElement[] | undefined> = ref()
+const props = defineProps<{
+    speed?: number
+    direction?: 'right' | 'left'
+}>()
+onMounted(() => {
+    if (itemsRef.value) {
+        items.value = Array.from(itemsRef.value?.children) as HTMLElement[]
+    }
+    items.value?.forEach(element => {
+        element.style.position = 'absolute'
+    });
+    setCord()
+    if (props.speed) {
+        rotate(props.speed)
+    }
+})
+
+function radianPositionDegree(position: number, N: number, offset: number = 0): number {
+    return position * (2 * Math.PI / N) - 0.5 * Math.PI + offset
+}
+
+function setPositionElement(degeree: number, element: HTMLElement, radius: number): void {
+    const elRect = element.getBoundingClientRect()
+    element.style.top = (radius * Math.sin(degeree) - elRect.height / 2) + 'px'
+    element.style.left = (radius * Math.cos(degeree) - elRect.width / 2) + 'px'
+}
+
+function setCord(offset?: number) {
+    items.value?.forEach((el, index) => {
+        if (containerRef.value) {
+            setPositionElement(radianPositionDegree(index, items.value?.length!!, offset), el, (containerRef.value.getBoundingClientRect().width / 2) - el.getBoundingClientRect().width/2)
+        }
+    })
+}
+function rotate(speedPercent: number) {
+    if (props.direction == 'left')
+    speedPercent = -speedPercent
+    let offset = 0
+    setInterval(() => {
+        setCord(offset += speedPercent/1200)
+    },20)
+}
+
+
+</script>
+
+<template>
+    <div class="rotate-circle relative items-center justify-center aspect-square" ref="containerRef">
+        <div class="items-center justify-center">
+            <slot name="center">
+
+            </slot>
+        </div>
+        <div class="absolute w-full h-full items-center justify-center">
+            <div class="relative w-[1px] h-[1px]" ref="itemsRef">
+                <slot name="items">
+
+                </slot>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped></style>
--- components/widgets/Stack.vue
+++ components/widgets/Stack.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { stack } from '~/constants/infoAboutMe';
+import RotateCircle from '../features/RotateCircle.vue';
 
 
 </script>
@@ -12,9 +13,42 @@ import { stack } from '~/constants/infoAboutMe';
         <ul class="flex flex-col gap-4 marker-list ">
             <li class="text-t-m" v-for="s in stack">{{ s }}</li>
         </ul>
+        <RotateCircle direction="left" :speed="10">
+            <template v-slot:center>
+                <div class="card-skill">
+                    <img src="@/assets/img/nuxt.svg" alt="">
+                </div>
+            </template>
+            <template v-slot:items>
+                <div class="card-skill">
+                    <img src="@/assets/img/html.png" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/js.svg" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/tailwind.svg" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/nest.png" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/ts.svg" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/saas.png" alt="">
+                </div>
+                <div class="card-skill">
+                    <img src="@/assets/img/postgre.svg" alt="">
+                </div>
+            </template>
+        </RotateCircle>
     </section>
 </template>
 
 <style scoped>
-
+.card-skill{
+    width: 80px;
+    height: 80px;
+}
 </style>
