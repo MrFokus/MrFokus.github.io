ad36a11f67b15525881a6c10bdd89c32324a7109
Глубоков Герман <glubokovgerman@MacBook-Pro.local>
2024-09-01 18:05:28 +0300

    feat: add ym

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr:false,
-  modules: ["@nuxtjs/tailwindcss"],
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss",
+    [
+      'yandex-metrika-module-nuxt3',
+      {
+        id: '98214979',
+        webvisor: true,
+        consoleLog: true,
+        clickmap: true,
+      }
+    ],
+  ],
   css: ['~/assets/styles/main.css']
 
 })
