3bf0604132c0e194172be54102a4d2bad46d428a
Глубоков Герман <glubokovgerman@MacBook-Pro.local>
2024-09-01 18:31:30 +0300

    fix: add ym

--- app.vue
+++ app.vue
@@ -18,28 +18,7 @@ watch(changeDark, () => {
 </script>
 
 <template>
-
-  <Head>
-    <!-- Yandex.Metrika counter -->
-    <Script type="text/javascript">
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(98214979, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</Script>
-    <noscript>
-      <div><img src="https://mc.yandex.ru/watch/98214979" style="position:absolute; left:-9999px;" alt="" /></div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
-  </Head>
+  
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: false,
-  modules: ["@nuxtjs/tailwindcss"],
+  ssr:false,
+  modules: ["@nuxtjs/tailwindcss",
+    [
+      'yandex-metrika-module-nuxt3',
+        {
+          id: '98214979',
+          webvisor: true,
+          // consoleLog: true,
+          // clickmap: true,
+          // useCDN: false,
+          // trackLinks: true,
+          // accurateTrackBounce: true,
+        }
+    ]
+  ],
   css: ['~/assets/styles/main.css']
 
 })
